f71afcbe6c1d23324a8034fd3845a7d6baf6feaf
unknown <jamicojerahmeel@gmail.com>
2025-08-19 03:05:53 +0100

    Add whitenoise for static files

--- store_project/settings.py
+++ store_project/settings.py
@@ -129,7 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 👈 required for collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # optional (if you have a /static folder in your project)
+
+# Enable Gzip + caching with Whitenoise
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
